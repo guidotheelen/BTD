28e6fcc8ff1646e12e745359f2ccffd0497c8dfc
Guido Theelen <guidotheelen@gmail.com>
2025-05-20 16:31:09 +0200

    first commit

---
+++ banner.js
@@ -0,0 +1,9 @@
+// banner.js
+const bannerHTML = `
+  <a href="index.html"><img src="assets/BTD-head.png" alt="Beyond The Doors Banner Logo" class="banner-logo small-logo" /></a>
+  <a href="mission.html">Mission</a>
+  <a href="methods.html">Methods</a>
+  <a href="experiences.html">Experiences</a>
+  <a href="contact.html">Contact</a>
+`;
+document.getElementById('banner').innerHTML = bannerHTML;
